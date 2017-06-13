90597e6acb1df9d4a0291d7b4b9d83bed47c60d7
Derrick <stjohndlee@gmail.com>
2017-06-13 19:46:19 +0900

    added POST

--- server/server.js
+++ server/server.js
@@ -23,6 +23,17 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    // setting it as an object makes it more flexible
+    res.send({todos: todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
