16118b61ac3f9683b5ed76e7848d09cff4f420f0
Derrick <stjohndlee@gmail.com>
2017-06-17 14:45:43 +0900

    added DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,16 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token: token}
+    }
+  });
+};
+
 // this is hwo you define a model method
 UserSchema.statics.findByToken = function(token) {
   var User = this;
