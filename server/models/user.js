66cfb42f1ce1e3ff530d1e9b1acf023de5bf60f6
Derrick <stjohndlee@gmail.com>
2017-06-17 18:01:28 +0900

    updated config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -50,7 +50,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
   // var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: this._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: this._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
   this.tokens.push({access, token});
   // returning a Promise that eventually gets resolved with the value of token
@@ -75,7 +75,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch(e) {
     return Promise.reject(); // just return a promise that rejects!
   }
